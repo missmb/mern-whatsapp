54efc0f70eb59e4cf7cc1316072e709fc82b3ca6
Mar'atul Bariroh <maratulbariroh3630@gmail.com>
2021-04-15 00:09:16 +0700

    create-server

--- server/server.js
+++ server/server.js
@@ -12,8 +12,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//DB config
+// app.use((req, res, next) =>{
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// });
 
+//DB config
 
 mongoose.connect("mongodb://localhost:12345/mern-whatsapp", {
   useNewUrlParser: true,
@@ -25,25 +30,32 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("mongodb database connection established succesfully");
 
-//   const msgCollection = connection.collection("messagecontents");
-//   const changeStream = msgCollection.watch();
-//   console.log(changeStream);
+  const msgCollection = connection.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+  console.log(changeStream);
 
-//   changeStream.on("change", (change) => {
-//     console.log("change", change);
+  // changeStream.on("change", (change) => {
+  //   console.log("change", change);
 
-//     if (change.operationType === "insert") {
-//       const messageDetails = change.fullDocument;
-//       pusher.trigger("message", "inserted", {
-//         name: messageDetails.user,
-//         message: messageDetails.message,
-//         timestamp:messageDetails.timestamp
-//       });
-//     }
-//   });
+    //     if (change.operationType === "insert") {
+    //       const messageDetails = change.fullDocument;
+    //       pusher.trigger("message", "inserted", {
+    //         name: messageDetails.user,
+    //         message: messageDetails.message,
+    //         timestamp:messageDetails.timestamp
+    //       });
+    //     }
+  // });
 });
 
 //???
+// const msgCollection = connection.collection("messagecontents");
+// const changeStream = Message.watch();
+// console.log(changeStream);
+
+// changeStream.on("change", change => {
+//   console.log("change", change);
+// });
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
